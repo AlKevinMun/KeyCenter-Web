fda882fdef753c41e25ccb17121b49000d735abf
HaolinZheng <hzheng@elpuig.xeill.net>
2024-05-16 16:43:50 +0200

    Axios

--- src/service/Axios.jsx
+++ src/service/Axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     //baseURL: 'http://192.168.242.12:8080/api',
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://192.168.250.84:8080/api',
     timeout: 5000,
     headers: {
         'Access-Control-Allow-Origin': '*',
