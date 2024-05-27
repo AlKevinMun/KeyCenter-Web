c5f13df5565bacb5c7fc1303d31c470d28043ca4
wavi <amunozgil@elpuig.xeill.net>
2024-05-27 16:17:45 +0200

    limpieza código

--- src/components/InputSelector.jsx
+++ src/components/InputSelector.jsx
@@ -14,89 +14,6 @@ function InputSelector(name, data, plural, onChange, onBlur, id) {
   let optionsToRender = [];
   if (id === 'Estados') {
     optionsToRender = options;
-
-    if (id === null) {
-      //Si se diera el caso de que la información dada por la api fuera NULL, se mostraria los datos por defecto.
-      if (data === null) {
-        return (
-          React.createElement('div', { className: 'input-group' },
-            React.createElement('label', { className: 'input-label' }, name,),
-            React.createElement('select', { className: 'input' },
-              React.createElement('option', { value: 'option1' }, 'option1'),
-              React.createElement('option', { value: 'option2' }, 'option2'),
-              React.createElement('option', { value: 'option3' }, 'option3')
-            )
-          )
-        );
-      }
-      else {
-        let options = [];
-        if (plural === null) {
-          if (name === 'Pais de residencia') {
-            options = data.paises.map((item, index) => {
-              return React.createElement('option', { key: item.id, value: item.id }, item.name);
-            });
-          } else if (name === 'Provincia') {
-            options = data.provincias.map((item, index) => {
-              return React.createElement('option', { key: item.paisId, value: item.paisId }, item.nombre);
-            });
-          } else if (name === 'Año de titulación' || name === 'Año de inicio') {
-            options = [];
-            for (let year = 2024; year >= 1924; year--) {
-              options.push(React.createElement('option', { key: year, value: year }, year));
-            }
-          } else if (name === 'Año de fin') {
-            options = [];
-            for (let year = 2024; year <= 2124; year++) {
-              options.push(React.createElement('option', { key: year, value: year }, year));
-            }
-          } else if (name === 'Mes de inicio' || name === 'Mes de fin') {
-            options = [];
-            const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-            meses.forEach((mes, index) => {
-              options.push(React.createElement('option', { key: index, value: mes }, mes));
-            });
-          }
-        }
-        else if (data.perfilprofesional === undefined) {
-          if (data.items.perfilprofesional[plural].listTypeEntries !== undefined) {
-            console.log(data.items.perfilprofesional[plural].listTypeEntries)
-            options = data.items.perfilprofesional[plural].listTypeEntries.map((item, index) => {
-              const nameI18n = item.nameI18n["es-ES"] || item.name;
-              return React.createElement('option', { key: item.id, value: item.id }, nameI18n);
-            });
-          }
-          else {
-            console.log(data.items.perfilprofesional[plural]);
-            data.items.perfilprofesional[plural].forEach(item => {
-              item.especialidades.map((especialidad, index) => {
-                let idEspecialidad = especialidad.id
-                let nombreEspecialidad = especialidad.name.name
-                options.push(React.createElement('option', { key: idEspecialidad, value: idEspecialidad }, nombreEspecialidad))
-              })
-            });
-          }
-        }
-
-        else {
-          console.log(data.perfilprofesional[plural].listTypeEntries);
-          options = data.perfilprofesional[plural].listTypeEntries.map((item, index) => {
-
-            const nameI18n = item.nameI18n["es-ES"] || item.name;
-            return React.createElement('option', { key: item.id, value: item.id }, nameI18n);
-          });
-        }
-
-        return (
-          React.createElement('div', { className: 'input-group' },
-            React.createElement('label', { className: 'input-label' }, name),
-            React.createElement('select', { className: 'input', name: name.toLowerCase(), onChange: onChange, onBlur: onBlur }, ...options)
-          )
-        );
-      }
-    }
-
-    else {
       if (id === 'Estados') {
         return (
           React.createElement('div', { className: 'input-group2', id: id },
@@ -107,18 +24,7 @@ function InputSelector(name, data, plural, onChange, onBlur, id) {
           )
         );
       }
-      return (
-        React.createElement('div', { className: 'input-group2', id: id },
-          React.createElement('label', { className: 'input-label' }, name,),
-          React.createElement('select', { className: 'input' },
-            React.createElement('option', { value: 'option1' }, 'option1'),
-            React.createElement('option', { value: 'option2' }, 'option2'),
-            React.createElement('option', { value: 'option3' }, 'option3')
-          )
-        )
-      );
     }
   }
-}
 export default InputSelector;
 
